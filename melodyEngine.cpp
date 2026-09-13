bf244f7e34097502277e0207f13976e7a8748322
Try Vithanuth <rothiolegolant@gmail.com>
2026-09-13 19:58:59 +0700

    changes

--- melodyEngine.cpp
+++ melodyEngine.cpp
@@ -3,10 +3,24 @@
 #include <string>
 #include <cstdlib>
 #include "core_functions.h"
+using namespace std;
 class MelodyEngine{
     //nuth
 };
+void interface(){
+    cout << "=============================" << endl;
+    cout << "        MelodyEngine" << endl;
+    cout << "=============================" << endl;
+    cout << endl;
+    cout << "Options (1-6): " << endl;
+    cout << "1. Play music" << endl;
+    cout << "2. Check Available Songs In a Playlist" << endl;
+    cout << "3. Check Playlist" << endl;
+    cout << "4." << endl;
+    cout << "5. " << endl;
+}
 int main(){
     //interface nuth
+    interface();
     return 0;
 }
