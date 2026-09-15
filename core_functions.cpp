1569313c70cc56c86fa85cdff1d2e33d0f6a8ef1
Kingoo <cheakhieng01@gmail.com>
2026-09-15 13:29:56 +0700

    My tasks kheangs

--- core_functions.cpp
+++ core_functions.cpp
@@ -18,8 +18,102 @@ class play_history_stack{
     //ravin
 };
 class play_up_next_queue{
-    //chikheang
+    //chikheang 
+    private:
+    QueueNode* front;   
+    QueueNode* rear;    
+    int count;          
+
+public:
+    
+    play_up_next_queue(){
+        front = nullptr;
+        rear = nullptr;
+        count = 0;
+    }
+
+    
+    bool isEmpty(){
+        return front == nullptr;
+    }
+
+    
+    void enqueue(Song s){
+        QueueNode* newNode = new QueueNode(s); 
+        if (isEmpty()){
+            
+            front = newNode;
+            rear = newNode;
+        } else {
+            // Otherwise, link it after the current last node, then move "rear" to it
+            rear->next = newNode;
+            rear = newNode;
+        }
+        count++; 
+    }
+
+   
+    Song dequeue(){
+        if (isEmpty()){
+            cout << "Up Next queue is empty!" << endl;
+            return Song{"", "", ""}; 
+        }
+
+        QueueNode* temp = front;   
+        Song playedSong = temp->song; 
+
+        front = front->next; 
+        if (front == nullptr){
+            
+
+            rear = nullptr;
+        }
+
+        delete temp; // Prevent memory leaks
+        count--;     
+        return playedSong;
+    }
+
+    
+    Song peek(){
+        if (isEmpty()){
+            cout << "Up Next queue is empty!" << endl;
+            return Song{"", "", ""};
+        }
+        return front->song;
+    }
+
+    int size(){
+        return count;
+    }
+
+
+    void display(){
+        if (isEmpty()){
+            cout << "No songs in Up Next." << endl;
+            return;
+        }
+        QueueNode* current = front; 
+        int position = 1;
+        cout << "----- Up Next -----" << endl;
+        while (current != nullptr){
+            cout << position << ". " << current->song.name
+                 << " - " << current->song.author << endl;
+            current = current->next; 
+            position++;
+        }
+    }
+
+    
+    ~play_up_next_queue(){
+        while (!isEmpty()){
+            dequeue();
+        }
+    }
+    //I didn't follow data stucture due to it's complicated to explain so I created another variable to navigate.
 };
+
+
 class playlist_linked_list{
     //sokheang
 public:
